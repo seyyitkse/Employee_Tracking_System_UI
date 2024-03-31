eced3834b533884e95069864067c6300ee82cdf3
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-31 15:30:18 +0300

    Create UI project.

--- UI_Layer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UI_Layer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
