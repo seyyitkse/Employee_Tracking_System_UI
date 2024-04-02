7904b11ac7ab88aff875a6f83f3b9c695ed0f957
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-04-02 09:44:12 +0300

    Created Admin Layout .

---
+++ UI_Layer/Controllers/Admin/AdminLayoutController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI_Layer.Controllers.Admin
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult AdminLayout()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}
