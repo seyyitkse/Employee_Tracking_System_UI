559f8d21224b9ad8a21120bb3effcbcbe15b0deb
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-05-06 23:15:00 +0300

    The main home page of the project has been created.

---
+++ UI_Layer/Controllers/HomeScreen/HomeScreenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI_Layer.Controllers.HomeScreen
+{
+    public class HomeScreenController : Controller
+    {
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
