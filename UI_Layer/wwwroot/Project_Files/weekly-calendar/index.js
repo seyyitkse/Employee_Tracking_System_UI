1122c3eae0fcc6e8c082ad8293c33a73421c17bc
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-05-16 10:30:05 +0300

    Work Schedule Process-3(With index.js file) Dynamic data extraction of the calendar has been enabled.(Now processing is done according to user id.)

--- UI_Layer/wwwroot/Project_Files/weekly-calendar/index.js
+++ UI_Layer/wwwroot/Project_Files/weekly-calendar/index.js
@@ -1,19 +1,31 @@
 import Calendar from './calendar.js';
 
-fetch('http://localhost:27312/api/ScheduleUser')
-    .then(response => response.json())
-    .then(events => {
-        const eventArray = events.map(event => {
-            return {
-                id: event.scheduleID,
-                eventName: event.description,
-                dateFrom: new Date(event.starttime).getTime(),
-                dateTo: new Date(event.endtime).getTime()
-            };
-        });
+const url = new URL(window.location.href);
+const id = url.searchParams.get('id');
 
-        new Calendar(eventArray);
-    })
-    .catch(error => {
-        console.error('Error fetching events:', error);
-    });
+if (id !== null) {
+    fetch(`http://localhost:27312/api/ScheduleUser/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(schedule => {
+            const eventArray = schedule.map(event => {
+                return {
+                    id: event.scheduleID,
+                    eventName: event.description,
+                    dateFrom: new Date(event.starttime).getTime(),
+                    dateTo: new Date(event.endtime).getTime()
+                };
+            });
+
+            new Calendar(eventArray);
+        })
+        .catch(error => {
+            console.error('Error fetching events:', error);
+        });
+} else {
+    console.error('No id parameter in the URL');
+}
