156df5d105205aae6d5154a2dc302f34e8c123c4
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-05-19 19:25:06 +0300

    Various interface arrangements-4 Necessary additions have been made so that the employee can see his/her own information. (Information is taken from JWT.) In addition, the employee was enabled to view system announcements.(Last 5 announcements)

--- UI_Layer/Controllers/Employee/EmployeeHomeController.cs
+++ UI_Layer/Controllers/Employee/EmployeeHomeController.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Net.Http;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using UI_Layer.Dtos.AnnouncementDto;
 using UI_Layer.Dtos.EmployeeDto;
 
 namespace UI_Layer.Controllers.Employee
@@ -29,6 +30,58 @@ namespace UI_Layer.Controllers.Employee
             {
                 return View();
             }
+        }
+        public IActionResult HomeEmployee()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Announcements()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:27312/api/Announcement");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAnnouncementDto>>(jsonData);
+                var lastFiveAnnouncements = values.OrderByDescending(a => a.Date).Take(5).ToList(); 
+                return View(lastFiveAnnouncements);
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> EmployeeInfo(int id)
+        {
+          var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:27312/api/Employee/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var info = JsonConvert.DeserializeObject<List<ResultEmployeeDto>>(jsonData);
+                return View(info);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult LogoutEmployee()
+        {
+            // Remove the authentication token
+            HttpContext.Response.Cookies.Delete("jwt");
+            // Redirect to the login page
+            return RedirectToAction("LoginEmployee", "LoginEmployee");
+        }
+        public static string GetUserIdFromToken(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(token);
+
+            // Assuming the user ID is stored in the "sub" claim
+            var userId = jwtToken.Claims.First(claim => claim.Type == "sub").Value;
+
+            return userId;
         }
     }
 }
