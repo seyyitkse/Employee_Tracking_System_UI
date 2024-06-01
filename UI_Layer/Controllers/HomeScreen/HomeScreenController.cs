463dd12ee8973ebfd07158a47a22c9f1fa281f86
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-06-01 21:38:23 +0300

    `Added HomePage and Login methods to HomeScreenController`
    ` `
    
    `In the HomeScreenController.cs file, part of the UI_Layer.Controllers.HomeScreen namespace, two new HttpGet methods have been added to the HomeScreenController class. The HomePage method returns a view when called, and the Login method also returns a view when invoked.`

--- UI_Layer/Controllers/HomeScreen/HomeScreenController.cs
+++ UI_Layer/Controllers/HomeScreen/HomeScreenController.cs
@@ -6,10 +6,12 @@ namespace UI_Layer.Controllers.HomeScreen
     [AllowAnonymous]
     public class HomeScreenController : Controller
     {
+        [HttpGet]
         public IActionResult HomePage()
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
