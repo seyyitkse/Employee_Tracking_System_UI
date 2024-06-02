dfc371a9d4a8611a2ad4f5943f1c17996a6a2724
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-06-02 20:35:25 +0300

    `Added new ResultNotificationDto class in UI_Layer`
    Added a new class `ResultNotificationDto` in the `UI_Layer.Dtos.NotificationsDto` namespace. This class includes properties such as `Id`, `Message`, `Time`, `Type`, `UserId`, and `Entry`. The `Message` and `Type` properties are nullable strings, while `Id`, `Time`, `UserId` are of type `int` and `Entry` is of type `bool`.

---
+++ UI_Layer/Dtos/NotificationsDto/ResultNotificationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace UI_Layer.Dtos.NotificationsDto
+{
+    public class ResultNotificationDto
+    {
+        public int Id { get; set; }
+        public string? Message { get; set; }
+        public long Time { get; set; }
+        public string? Type { get; set; }
+        public int UserId { get; set; }
+        public bool Entry { get; set; }
+    }
+}
