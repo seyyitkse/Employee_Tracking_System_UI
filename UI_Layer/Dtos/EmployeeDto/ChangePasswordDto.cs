57b4f29969e774d4615eb54f6fc457fa203f54a8
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-07-12 20:48:14 +0300

    Add ChangePassword feature for employees
    - Introduced `ChangePasswordDto` in `UI_Layer.Dtos.EmployeeDto` with required properties: `Email`, `CurrentPassword`, and `NewPassword`. The `Email` property is of type `EmailAddress`, and password properties are of type `Password`.
    - Added `ChangePassword.cshtml` Razor view, utilizing `ChangePasswordDto` as its model. This view includes a form for password change, a notification section for messages, and specifies `EmployeeLayout.cshtml` as its layout.
    - Implemented AJAX in `@section Scripts` for form submission to handle password change without page reload. This includes form data collection, JWT token parsing for email extraction, and sending an AJAX POST request to `ChangePassword` API endpoint with authentication token. It also dynamically updates notification messages based on the operation's success or failure.

---
+++ UI_Layer/Dtos/EmployeeDto/ChangePasswordDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UI_Layer.Dtos.EmployeeDto
+{
+    public class ChangePasswordDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+    }
+}
